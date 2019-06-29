cbc4d289b530fb6811ee6f87968b8dda9d81c64b
Nivedha11 <52134061+Nivedha11@users.noreply.github.com>
2019-06-29 19:53:16 +0530

    Create b3.py

---
+++ b3.py
@@ -0,0 +1,7 @@
+chr=input(" ")
+if chr in('a','e','i','o','u'):
+    print("Vowel")
+elif chr in('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+    print("Consonant")
+else:
+    print("invalid")
