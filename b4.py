8ca3c60b182b514ee914a6597a6815e037665c31
Nivedha11 <52134061+Nivedha11@users.noreply.github.com>
2019-07-05 20:00:05 +0530

    Update b4.py

--- b4.py
+++ b4.py
@@ -1,6 +1,6 @@
-char1=input(" ")
+charr=input(" ")
 alp=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-if char1 in alp:
+if charr in alp:
     print("Alphabet")
 else:
     print("No")
