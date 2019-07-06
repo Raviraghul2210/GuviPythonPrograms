1864da46403a6e31352fe0cedae6b1147c0bf595
Nivedha11 <52134061+Nivedha11@users.noreply.github.com>
2019-07-06 08:50:52 +0530

    Create b18.py

---
+++ b18.py
@@ -0,0 +1,10 @@
+k,n=map(int,input().split())
+for m in range(k+1,n):
+    sum=0
+    num=m
+    while num>0:
+        dig=num%10
+        sum+=dig**3
+        num//=10
+        if(m==sum):
+            print(m,end=" ")
