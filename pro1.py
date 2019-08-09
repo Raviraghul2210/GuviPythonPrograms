d0816061967bc50e39bb05ffb1350637950d0015
Raviraghul2210 <52314446+Raviraghul2210@users.noreply.github.com>
2019-08-09 15:00:09 +0530

    Create pro1.py

---
+++ pro1.py
@@ -0,0 +1,12 @@
+sum=int(input())
+p=[]
+for x in range(0,sum):
+ la=input()
+ p.append(la)
+new=[]
+for x in zip(*p):
+ if(x.count(x[0])==len(x)):
+  new.append(x[0])
+ else:
+  break
+print(''.join(new))
